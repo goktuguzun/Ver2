158fee53e14ae3cb8172c6149226beca0718e52a
guzun <goktuguzun@outlook.com.tr>
2022-05-09 19:20:45 +0300

    Ver2

---
+++ StokWeb/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokWeb.Models.Entitiy;
+using System.Web.Security;
+
+namespace StokWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        StokTakipEntities1 db = new StokTakipEntities1();
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(WebUser u)
+        {
+
+            var deger = db.WebUser.FirstOrDefault(x => x.UserName == u.UserName && x.Pass == u.Pass);
+
+            if (deger!=null)
+            {
+                FormsAuthentication.SetAuthCookie(deger.UserName, false);
+                return RedirectToAction("Index","Main");
+            }
+            else
+            {
+                
+                return View();
+            }
+           
+        }
+    }
+}
---
+++ StokWeb/Controllers/ServisController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokWeb.Models.Entitiy;
+namespace StokWeb.Controllers
+{
+    public class ServisController : Controller
+    {
+        // GET: Servis
+        StokTakipEntities1 db = new StokTakipEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.Servis.ToList();
+            return View(degerler);
+        }
+
+
+        public ActionResult ServisSil(int id)
+        {
+            var stok = db.Servis.Find(id);
+            db.Servis.Remove(stok);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
